854386840b8843f1f2f554d6d461d56156fac3aa
JsTHiago <thiagobaldino5@gmail.com>
2021-02-14 18:49:40 -0300

    feito

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,15 +1,41 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
-class CreateTransactionService {
+
+interface Request{
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+
+}
+
+
+
+class CreateTransactionService{
   private transactionsRepository: TransactionsRepository;
 
   constructor(transactionsRepository: TransactionsRepository) {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({title,value,type}:Request): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+    console.log(balance['total'] - value)
+    if(balance['total'] - value < 0 && type === 'outcome'){
+      throw Error('Cannot withdraw money');
+    }
+    if(type === "income"){
+      balance['income'] += value;
+    }else{
+      balance['outcome'] += value;
+    }
+
+    balance['total'] = balance['income'] - balance['outcome'];
+
+      const transaction = this.transactionsRepository.create({title,type,value});
+      return transaction;
   }
 }
 
